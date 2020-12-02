7af0f67647bcec2ea29822ba68e9d09cfc77f502
Manu Cheremeh <manucheremeh1995@gmail.com>
2020-12-02 16:14:46 +0000

    form error validation

---
+++ form_errors.rb
@@ -0,0 +1,19 @@
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance_tag|
+  fragment = Nokogiri::HTML::DocumentFragment.parse(html_tag)
+  field = fragemt.at('input,select,textarea')
+  model = instance_tag.object
+  error_message = model.errors.full_messages.join(', ')
+  
+  html = if field
+            field['class'] = "#{field['class']} invalid"
+            html = <<-HTML
+            #{fragment.to_s}
+            <p class="text-danger">#{error_message}</p>
+            HTML
+            html
+         else
+            html_tag
+         end
+
+  html.html_safe
+end
